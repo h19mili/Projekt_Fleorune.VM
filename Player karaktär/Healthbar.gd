1c8515a97b63daa8f0f5fd82fba45afa92f81988
AxeDennis <frej_d94@hotmail.com>
2021-01-22 11:37:04 +0100

    Copy

---
+++ Player karaktär/Healthbar.gd
@@ -0,0 +1,22 @@
+extends TextureProgress
+
+onready var Max_HP = get_node("..").Max_HP
+onready var Current_HP = get_node("..").Current_HP
+
+
+func _ready():
+	_Healthbar()
+	pass # Replace with function body.
+
+
+func _Healthbar():
+	self.max_value = Max_HP
+	self.value = Current_HP
+	pass
+ 
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		Current_HP -= 1
+		_Healthbar()
+		print(Current_HP)
---
+++ TurnQ.gd
@@ -0,0 +1,44 @@
+extends YSort
+
+class_name TurnQ
+signal completed
+onready var active_player = Battler
+
+func _ready():
+	initialize()
+	pass
+
+func initialize():
+	var Battlers = get_players()
+	print("BATTLERS: ")
+	print(Battlers)
+	Battlers.sort_custom(self, 'sort_players')
+	Battlers[0].raise()
+	print("BATTLERS efter: ")
+	print(Battlers)
+	active_player = get_child(0)
+	play_turn()
+
+func play_turn():
+	yield(active_player, "completed")
+	print("fight")
+	var new_index : int = (active_player.get_index() + 1) % get_child_count()
+	active_player = get_child(new_index)
+	yield(active_player, "completed")
+	print("again")
+	_next_battler()
+	play_turn()
+
+static func sort_players(a : Battler, b : Battler) -> bool:
+	return a.Speed > b.Speed
+
+func _next_battler():
+	var new_index : int = (active_player.get_index() + 1) % get_child_count()
+	active_player = get_child(new_index)
+
+func get_players():
+	return get_children()
+
+func _Done():
+	emit_signal("completed")
+	pass 
